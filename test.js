6dbb574882895895bd722c85b5309717dd6c1934
elidotco <eddiefestde@gmail.com>
2025-08-15 18:00:16 +0000

    changes

--- test.js
+++ test.js
@@ -11,6 +11,7 @@ console.log("To:", process.env.BOOKING_EMAIL);
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
+
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
